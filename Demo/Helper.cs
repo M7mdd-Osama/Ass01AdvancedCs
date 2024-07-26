db3df3f65dbc4bfe3a4dbb45d4a3e692e7877ea7
Mohamed Osama Ali <ana20332183@gmail.com>
2024-07-27 02:28:15 +0300

    Generics

---
+++ Demo/Helper.cs
@@ -0,0 +1,40 @@
+﻿using Demo.Generics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Helper /*<T>*/
+    {
+        #region non Generics
+        //public static void SWAP(ref int X, ref int Y)
+        //{
+        //    int Temp = X;
+        //    X = Y;
+        //    Y = Temp;
+        //}
+        //public static void SWAP(ref double X, ref double Y)
+        //{
+        //    double Temp = X;
+        //    X = Y;
+        //    Y = Temp;
+        //}
+        //public static void SWAP(ref Point X, ref Point Y)
+        //{
+        //    Point Temp = X;
+        //    X = Y;
+        //    Y = Temp;
+        //} 
+        #endregion
+        public static void SWAP<T>(ref T X, ref T Y)
+        {
+            T Temp = X;
+            X = Y;
+            Y = Temp;
+        }
+
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,9 +1,28 @@
-﻿namespace Demo
+﻿using Demo.Generics;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region non Generics
+            //int A = 5, B = 7;
+            //Console.WriteLine($"A = {A} , B = {B}");
+            //Helper.SWAP(ref A,ref B);
+            //Console.WriteLine($"A = {A} , B = {B}");
+            //Console.WriteLine("================");
+            //double X = 5.2, Y = 7.5;
+            //Console.WriteLine($"A = {X} , B = {Y}");
+            //Helper.SWAP(ref X,ref Y);
+            //Console.WriteLine($"A = {X} , B = {Y}");
+
+            //Point point = new Point(10, 3);
+            //Point point2 = new Point(2, 4);
+            //Helper.SWAP(ref point,ref point2);
+            //Console.WriteLine($"{point}  \n {point2}"); 
+            #endregion
+
 
         }
     }
