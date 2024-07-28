6623c422b497cef4f28ffc966a95d5583773b296
Mohamed Osama Ali <ana20332183@gmail.com>
2024-07-28 17:35:13 +0300

    Q1

---
+++ Assignment/Helper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class Helper
+    {
+        public static void SWAP<T>(ref T X, ref T Y)
+        {
+            T Temp = X;
+            X = Y;
+            Y = Temp;
+        }
+
+        public static void BubbleSort(int[] array)
+        {
+            if (array is not null)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    bool swapped = false;
+                    for (int K = 0; K < array.Length - i - 1; K++)
+                    {
+                        if (array[K] > array[K + 1])
+                        {
+                            SWAP<int>(ref array[K], ref array[K + 1]);
+                            swapped = true;
+                        }
+                    }
+                    if (!swapped)
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = { 3, 0, 5, 4, 2, 4, 6, 8, 7, 6, 1 };
+
+            Helper.BubbleSort(Numbers);
+
+            foreach (int i in Numbers)
+            {
+                Console.Write($"{i} " );
+            }
+        }
+    }
+}
